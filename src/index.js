df26d226b21fc2dba40975497bec3345bb322f9e
Lyubomir Atanasov <Lyubomir.Atanasov@dskbank.bg>
2023-09-18 14:54:06 +0300

    Added HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import './i18n';
 
@@ -12,8 +12,8 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
