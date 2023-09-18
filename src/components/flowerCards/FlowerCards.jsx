59a8feec2f985b936e94cc9ae2ab4ff0a8d48b60
Lyubomir Atanasov <Lyubomir.Atanasov@dskbank.bg>
2023-09-18 16:41:18 +0300

    Modified styles of flover cards

--- src/components/flowerCards/FlowerCards.jsx
+++ src/components/flowerCards/FlowerCards.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
 import boadImage from "../../assets/images/boadImage.PNG";
@@ -6,7 +7,66 @@ import routes from "../../util/routes";
 
 import styles from "./FlowerCards.module.scss";
 
+const dataCards = [
+  {
+    id: 0,
+    name: 'Sweet & Pretty Bouquet',
+    price: '45лв'
+  },
+  {
+    id: 1,
+    name: 'Sweet & Pretty Bouquet',
+    price: '2лв'
+  },
+  {
+    id: 2,
+    name: 'Sweet & Pretty Bouquet',
+    price: '45лв'
+  },
+  {
+    id: 3,
+    name: 'Sweet & Pretty Bouquet',
+    price: '45лв'
+  },
+  {
+    id: 4,
+    name: 'Sweet & Pretty Bouquet',
+    price: '412лв'
+  },
+  {
+    id: 5,
+    name: 'Sweet & Pretty Bouquet',
+    price: '45лв'
+  },
+  {
+    id: 6,
+    name: 'Sweet & Pretty Bouquet',
+    price: '45лв'
+  },
+  {
+    id: 7,
+    name: 'Sweet & Pretty Bouquet',
+    price: '4лв'
+  },
+  {
+    id: 8,
+    name: 'Sweet & Pretty Bouquet',
+    price: '45лв'
+  },
+  {
+    id: 9,
+    name: 'Sweet & Pretty Bouquet',
+    price: '145лв'
+  },
+  {
+    id: 10,
+    name: 'Sweet & Pretty Bouquet',
+    price: '45лв'
+  }
+];
+
 const FlowerCards = ({ route }) => {
+
   const habdleImage = () => {
     switch (route) {
     case routes.romantic:
@@ -17,59 +77,17 @@ const FlowerCards = ({ route }) => {
   };
 
   return (
-    <>
-      <div className={styles.flowers}>
-        <div className={styles.flowerOne}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-        <div className={styles.flowerTwo}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-        <div className={styles.flowerThree}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-      </div>
-      <div className={styles.flowers}>
-        <div className={styles.flowerOne}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-        <div className={styles.flowerTwo}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-        <div className={styles.flowerThree}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-      </div>
-      <div className={styles.flowers}>
-        <div className={styles.flowerOne}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-        <div className={styles.flowerTwo}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-        <div className={styles.flowerThree}>
-          <img src={habdleImage()} alt="" />
-          <p className={styles.name}>Sweet & Pretty Bouquet</p>
-          <p className={styles.price}>45лв</p>
-        </div>
-      </div>
-    </>
+    <div className={styles.flowers}>
+      {
+        dataCards.map(flower => {
+          return <div key={flower.id} className={styles.flower}>
+            <img src={habdleImage()} alt="" />
+            <p className={styles.name}>{flower.name}</p>
+            <p className={styles.price}>{flower.price}</p>
+          </div>;
+        })
+      }
+    </div>
   );
 };
 
